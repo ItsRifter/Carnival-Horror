358e65cb43b5bfbf4610ceab3a033a885e1141ef
ItsRifter <d_thomas_smith30@hotmail.com>
2023-02-23 14:24:35 +0000

    Distractive Item 1/2

---
+++ Assets/Scripts/Interactive/DistractiveItem.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistractiveItem : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask mask;
+
+    [SerializeField]
+    float minVelocity = -1.5f;
+
+    [SerializeField]
+    float distractRange = 64.0f;
+
+    Rigidbody rb;
+    bool canDistract;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //If the object is falling at the required velocity, set boolean to true
+        if(rb.velocity.y < minVelocity && !canDistract)
+            canDistract = true;
+
+        //if we just hit the ground and the object can distract
+        //find the AI and distract them
+        //then set the boolean to false
+        if(rb.velocity.y > 0.1f && canDistract)
+        {
+            foreach (var obj in Physics.OverlapSphere(transform.position, distractRange))
+            {
+                
+            }
+
+            canDistract = false;
+        }
+    }
+}
--- Assets/Scripts/Interactive/GrabAndThrow.cs
+++ Assets/Scripts/Interactive/GrabAndThrow.cs
@@ -18,21 +18,29 @@ public class GrabAndThrow : MonoBehaviour
 
     void Update()
     {
+        //If the left mouse is pressed
         if( Input.GetKeyDown(KeyCode.Mouse0) )
         {
+            //If we aren't grabbing an object, get the raycasted object
             if ( grabbed == null )
                 grabbed = GetGrabbed();
+            //Otherwise just nullify what we have grabbed
             else
                 grabbed = null;
         }
 
+        //While we have a grabbed object
         if( grabbed != null )
         {
+            //Set the position to the camera position and forward vector by 25
+            //Also set the grabbed objects velocity to zero so it doesn't think its falling
             grabbed.transform.position = eyeCam.transform.position + (eyeCam.transform.forward * 2.5f);
             grabbed.velocity = Vector3.zero;
 
+            //If the right mouse is pressed
             if(Input.GetKeyDown(KeyCode.Mouse1))
             {
+                //Throw the object with force at forward vector of camera and nullify grabbed
                 grabbed.AddForce(eyeCam.transform.forward * throwForce);
                 grabbed = null;
             }
