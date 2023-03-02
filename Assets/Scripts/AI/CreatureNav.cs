329451e49d5f59c0ef3810e7954929805200ed50
Shadow-Striker <82645925+Shadow-Striker@users.noreply.github.com>
2023-03-02 09:30:53 +0000

    add chase player mechanic for creature

--- Assets/Scripts/AI/CreatureNav.cs
+++ Assets/Scripts/AI/CreatureNav.cs
@@ -6,36 +6,59 @@ using UnityEngine.AI;
 public class CreatureNav : MonoBehaviour
 {
     public NavMeshAgent agent;
-    private float searchRadius;
+    private float randomSearchRadius;
+    public float hearingRadius;
     public Vector3 destination;
     public bool randomDestinationFound;
     public bool hasReachedDestination;
     public Transform playerTransform;
     public float maxWaitTime;
     public float waitTimeLeft;
+    public SphereCollider sphereCollider;
+    public bool canHearPlayer;
+    public Vector3 lastHeardPlayerPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         waitTimeLeft = maxWaitTime;
-        searchRadius = agent.height * 20.0f;
+        randomSearchRadius = agent.height * 20.0f;
+        sphereCollider.radius = hearingRadius;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        //print("Agent path: " + agent.pathStatus);
-        if (!randomDestinationFound && CheckForRandomNavMeshPoint(playerTransform.position, searchRadius, out destination))
+    { 
+        //Find a random point to go to if we haven't already.
+        if (!randomDestinationFound && CheckForRandomNavMeshPoint(playerTransform.position, randomSearchRadius, out destination))
         {
             randomDestinationFound = true;
         }
         else
         {
+            //Move to destination then wait for specified period of time once we've reached it.
             agent.SetDestination(destination);
 
             if (agent.remainingDistance <= 1.0f)
             {
                 Wait();
+            }
+        }
+
+        if(Vector3.Distance(transform.position, playerTransform.position) <= hearingRadius)
+        {
+            print("Moving to last heard player position");
+            lastHeardPlayerPosition = playerTransform.position;
+            canHearPlayer = true;
+        }
+
+        if(canHearPlayer)
+        {
+            agent.SetDestination(lastHeardPlayerPosition);
+
+            if(agent.remainingDistance <= 1.0f && Vector3.Distance(transform.position, playerTransform.position) > hearingRadius)
+            {
+                canHearPlayer = false;
             }
         }
     }
@@ -68,7 +91,7 @@ public class CreatureNav : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(playerTransform.position, searchRadius);
+        Gizmos.DrawWireSphere(playerTransform.position, randomSearchRadius);
 
     }
 }
